7d0c5c1afcf1191b087cac6690efbecd99985cc3
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-16 12:28:40 +0300

    register the models in the admin dashoard

--- rating/admin.py
+++ rating/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 from .models import Profile,Project,Rating,technologies,categories
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    filter_horizontal = ('technologies', 'categories')
+
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Rating)
+admin.site.register(categories)
+admin.site.register(technologies)
