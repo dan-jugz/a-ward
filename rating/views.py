3c62d07cdd15373bcb42981ffa40d5c9b506a510
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-16 13:14:08 +0300

    setup another api view for profile and project descriptions

--- rating/views.py
+++ rating/views.py
@@ -162,3 +162,32 @@ class ProjectList(APIView):
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProfileDescription(APIView):
+
+    def get_profile(self, pk):
+        try:
+            return Profile.objects.get(pk=pk)
+        except Profile.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        profile = self.get_profile(pk)
+        serializers = ProfileSerializer(profile)
+        return Response(serializers.data)
+
+
+class ProjectDescription(APIView):
+    
+    def get_project(self, pk):
+        try:
+            return Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        project = self.get_project(pk)
+        serializers = ProjectSerializer(project)
+        return Response(serializers.data)
+        
