4a96428d213efe2629adf60a19628c8f7fe12c17
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-16 12:24:08 +0300

    start the ratings app to carry out the rating functionality

---
+++ rating/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from .models import Profile,Project,Rating,technologies,categories
+# Register your models here.
---
+++ rating/forms.py
@@ -0,0 +1,2 @@
+from django import forms
+from .models import Project, Profile, Rating
