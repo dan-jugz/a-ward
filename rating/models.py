a6a4bff760bfa67264e933d181998892bc7ef151
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-16 12:26:37 +0300

    setup the project model to hold project parameters

--- rating/models.py
+++ rating/models.py
@@ -29,3 +29,28 @@ class technologies(models.Model):
     def delete_technology(cls,technologies):
         cls.objects.filter(technologies=technologies).delete()
 
+
+class Project(models.Model):
+    title = models.CharField(max_length=150)
+    landing_page = models.ImageField(upload_to='projects/')
+    description = models.CharField(max_length=255)
+    live_link = models.CharField(max_length=255)
+    design = models.IntegerField(blank=True,default=0)
+    usability = models.IntegerField(blank=True,default=0)
+    creativity = models.IntegerField(blank=True,default=0)
+    content = models.IntegerField(blank=True,default=0)
+    overall = models.IntegerField(blank=True,default=0)
+    categories = models.ManyToManyField(categories)
+    technologies = models.ManyToManyField(technologies)
+    posted  = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+    @classmethod
+    def search_project(cls, search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        return projects
+    
