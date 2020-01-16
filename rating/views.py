192561c2cfbbb40b8e76e2319940c2bd8db2b7e0
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-16 12:38:53 +0300

    define a function that renders the homepage of the appliication

--- rating/views.py
+++ rating/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
+from django.contrib.auth.decorators import login_required
+from .models  import Project,Profile,Rating,categories,technologies
+from .forms import ProfileForm, UploadForm, RatingForm
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+@login_required(login_url='/accounts/login')
+def index(request):
+    current_user = request.user 
+    projects = Project.objects.order_by('-overall').all()
+    
+    runners=Project.objects.all()[:4]
+    try:
+        current_user = request.user
+        profile =Profile.objects.get(user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('edit')
+    return render(request, 'index.html', locals())
