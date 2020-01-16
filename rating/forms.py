04e2b92332d7b6536a3078639d4c13ddd0870789
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-16 12:31:29 +0300

    add a form to upload projects

--- rating/forms.py
+++ rating/forms.py
@@ -1,2 +1,7 @@
 from django import forms
 from .models import Project, Profile, Rating
+
+class UploadForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ('design', 'usability', 'creativity', 'content', 'overall', 'posted', 'user' )
