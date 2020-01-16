cffd66d638272b13c85b9943fab971de3fccbc5d
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-16 13:01:15 +0300

    write a function to render the project and its ratings

--- rating/views.py
+++ rating/views.py
@@ -42,3 +42,97 @@ def edit_profile(request):
     else:
         form = ProfileForm()
     return render(request, 'edit_profile.html', {'form': form, 'profile':profile})
+
+@login_required(login_url='/accounts/login')
+def new_project(request):
+    current_user = request.user
+    profile =Profile.objects.get(user=current_user)
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            image.save()
+            return redirect('index')
+    else:
+        form = UploadForm()
+    return render(request, 'new_project.html',  {'form': form,'profile':profile})
+
+
+@login_required(login_url='/accounts/login')
+def project(request,project_id):
+    current_user = request.user
+
+    profile =Profile.objects.get(user=current_user)
+    message = "Thank you for voting"
+    try:
+        project = Project.objects.get(id=project_id)
+    except Project.DoesNotExist:
+        raise ObjectDoesNotExist()
+
+   
+    total_design = 0
+    total_usability = 0 
+    total_creativity = 0
+    total_content = 0
+    overall_score = 0
+
+    ratings = Rating.objects.filter(project=project_id)
+
+    if len(ratings) > 0:
+        users = len(ratings)
+    else:
+        users = 1
+    
+    design = list(Rating.objects.filter(project=project_id).values_list('design',flat=True))
+    usability = list(Rating.objects.filter(project=project_id).values_list('usability',flat=True))
+    creativity = list(Rating.objects.filter(project=project_id).values_list('creativity',flat=True))
+    content = list(Rating.objects.filter(project=project_id).values_list('content',flat=True))
+    
+    
+    total_design=sum(design)/users
+    total_usability=sum(usability)/users
+    total_creativity=sum(creativity)/users
+    total_content=sum(content)/users
+
+
+    overall_score=(total_design+total_content+total_usability+total_creativity)/4
+
+    project.design = total_design
+    project.usability = total_usability
+    project.creativity = total_creativity
+    project.content = total_content
+    project.overall = overall_score
+
+    project.save()
+
+    if request.method == 'POST':
+        form = RatingForm(request.POST, request.FILES) 
+        if form.is_valid():
+            rating = form.save(commit=False)
+            rating.project= project
+            rating.profile = profile
+            if not Rating.objects.filter(profile=profile, project=project).exists():
+                rating.overall_score = (rating.design+rating.usability+rating.creativity+rating.content)/4
+                rating.save()
+    else:
+        form = RatingForm()
+    return render(request, "project.html",{"project":project,"profile":profile,"ratings":ratings,"form":form, "message":message, 'total_design':total_design, 'total_usability':total_usability, 'total_creativity':total_creativity, 'total_content':total_content})
+
+
+
+@login_required(login_url='/accounts/login')
+def search(request):
+    current_user = request.user
+    profile =Profile.objects.get(user=current_user)
+    if 'project' in request.GET and request.GET["project"]:
+        search_term = request.GET.get("project")
+        projects = Project.search_project(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message":message, "projects":projects, 'profile':profile})
+    else:
+        message = "Please enter search term"
+        return render(request, 'search.html', {"message":message, "projects":projects,'profile':profile})
+
+
