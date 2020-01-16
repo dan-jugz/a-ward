97b252ecced995abb87d1991727b9f816e0c9854
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-16 12:32:01 +0300

    add a form for the user profile

--- rating/forms.py
+++ rating/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Project, Profile, Rating
 
+
 class UploadForm(forms.ModelForm):
     class Meta:
         model = Project
         exclude = ('design', 'usability', 'creativity', 'content', 'overall', 'posted', 'user' )
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('profilepic','bio', 'prefname', 'contact')
