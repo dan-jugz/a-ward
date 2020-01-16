3256bbf3a730613ae11888c64d192695f0eab631
daniel njuguna <njugunadaniel364@gmail.com>
2020-01-16 12:32:30 +0300

    setup a ratings form for the webpage

--- rating/forms.py
+++ rating/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Project, Profile, Rating
 
+RATING= [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 class UploadForm(forms.ModelForm):
     class Meta:
@@ -11,3 +12,9 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('profilepic','bio', 'prefname', 'contact')
+
+class RatingForm(forms.ModelForm):
+    class Meta:
+        model=Rating
+        exclude=['overall_score','profile','project']
+        
